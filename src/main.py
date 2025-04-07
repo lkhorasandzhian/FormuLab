4b973555093b97be24774b8cd42dab9b3960c3b1
Levon Khorasandzhian <levon.kh228@gmail.com>
2025-04-07 05:26:12 +0300

    Add filter warnings for start point, closed #11

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 import tkinter as tk
+import warnings
+
+from nbformat.warnings import MissingIDFieldWarning
 
 from src.formulab_application import FormuLabApplication
 
 # Запуск приложения.
 if __name__ == "__main__":
+    warnings.filterwarnings("ignore", category=MissingIDFieldWarning)
+    warnings.filterwarnings("ignore", message="IPython3 lexer unavailable, falling back on Python 3")
     root = tk.Tk()
     app = FormuLabApplication(root)
     app.launch()
